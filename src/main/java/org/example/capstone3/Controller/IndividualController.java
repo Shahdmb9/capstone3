17cea88cba49960cb1e1aa78b91fb01a53ffdba3
Reenad <ren1424h@gmail.com>
2026-06-03 09:34:06 +0300

    Initial Commit - Add individual flow CRUD and models

--- src/main/java/org/example/capstone3/Controller/IndividualController.java
+++ src/main/java/org/example/capstone3/Controller/IndividualController.java
@@ -1,4 +1,63 @@
 package org.example.capstone3.Controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.capstone3.API.ApiResponse;
+import org.example.capstone3.Models.HealthProfile;
+import org.example.capstone3.Models.Individual;
+import org.example.capstone3.Service.IndividualService;
+import org.example.capstone3.Service.HealthProfileService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/individual")
+@RequiredArgsConstructor
 public class IndividualController {
+
+    private final IndividualService individualService;
+    private final HealthProfileService healthProfileService;
+
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAllIndividuals() {
+        return ResponseEntity.status(200).body(individualService.getAllIndividuals());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> registerIndividual(@RequestBody Individual individual) {
+        individualService.addIndividual(individual);
+        return ResponseEntity.status(201).body(new ApiResponse("Individual registered successfully"));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponse> updateIndividual(@PathVariable Integer id, @RequestBody Individual individual) {
+        individualService.updateIndividual(id, individual);
+        return ResponseEntity.status(200).body(new ApiResponse("Individual updated successfully"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse> deleteIndividual(@PathVariable Integer id) {
+        individualService.deleteIndividual(id);
+        return ResponseEntity.status(200).body(new ApiResponse("Individual deleted successfully"));
+    }
+
+
+    @PostMapping("/profile/add/{individualId}")
+    public ResponseEntity<ApiResponse> addHealthProfile(@RequestBody HealthProfile profile, @PathVariable Integer individualId) {
+        healthProfileService.addProfile(profile, individualId);
+        return ResponseEntity.status(201).body(new ApiResponse("Health Profile added successfully"));
+    }
+
+    @PutMapping("/profile/update/{individualId}")
+    public ResponseEntity<ApiResponse> updateHealthProfile(@PathVariable Integer individualId, @RequestBody HealthProfile profile) {
+        healthProfileService.updateProfile(individualId, profile);
+        return ResponseEntity.status(200).body(new ApiResponse("Health Profile updated successfully"));
+    }
+
+    @DeleteMapping("/profile/delete/{individualId}")
+    public ResponseEntity<ApiResponse> deleteHealthProfile(@PathVariable Integer individualId) {
+        healthProfileService.deleteProfile(individualId);
+        return ResponseEntity.status(200).body(new ApiResponse("Health Profile deleted successfully"));
+    }
 }
--- src/main/java/org/example/capstone3/Repository/HealthProfileRepository.java
+++ src/main/java/org/example/capstone3/Repository/HealthProfileRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HealthProfileRepository extends JpaRepository<HealthProfile, Integer> {
-    HealthProfile findByIndividualId(Integer id);
+    HealthProfile findHealthProfileById(Integer id);
 }
 
